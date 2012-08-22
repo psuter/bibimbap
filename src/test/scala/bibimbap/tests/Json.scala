795d604acc4e1d6a65eae7c4548d43850014d656
Philippe Suter <philippe.suter@gmail.com>
2012-08-22 20:33:41 +0200

    more json testing

--- src/test/scala/bibimbap/tests/Json.scala
+++ src/test/scala/bibimbap/tests/Json.scala
@@ -5,7 +5,7 @@ import bibimbap.json._
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
-class JsonUsage extends FunSuite with ShouldMatchers {
+class JsonParsing extends FunSuite with ShouldMatchers {
   private def parsesAs(in : String, v : JValue) {
     (new JsonParser).parse(in) should equal (v)
   }
@@ -13,6 +13,26 @@ class JsonUsage extends FunSuite with ShouldMatchers {
   test("Base cases") {
     parsesAs("""null""", JNull)
 
+    parsesAs(""" true""", JTrue)
+
+    parsesAs("""false """, JFalse)
+
     parsesAs("""42""", JInt(42))
+
+    parsesAs("""[ 1, 2, 3 ]""", JArray(JInt(1) :: JInt(2) :: JInt(3) :: Nil))
+
+    parsesAs("""{
+      "foo" : 42,
+      "bar" : "bar"
+    }""", JObject(Map("foo" -> JInt(42), "bar" -> JString("bar"))))
+  }
+
+  test("Selectors") {
+    val needle = JString("needle")
+    val stack0 = JObject(Map("bar" -> needle))
+    val stack1 = JArray(JInt(4) :: JInt(2) :: Nil)
+    val haystack = JObject(Map("fuu" -> stack1, "foo" -> stack0))
+
+    (haystack \ "foo" \ "bar") should equal (needle)
   }
 }
